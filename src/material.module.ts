953ed961e2008be16b7b18fd4c8207a607b6ddf3
Ben <bheathit@gmail.com>
2018-10-29 19:40:34 -0400

    Cleared project for restart

--- src/material.module.ts
+++
@@ -1,9 +0,0 @@
-import {MatButtonModule, MatCheckboxModule, MatCardHeader, MatGridListModule} from "@angular/material";
-import { NgModule } from "@angular/core";
-
-
-@NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatCardHeader, MatGridListModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatCardHeader, MatGridListModule],
-})
-export class MyMaterialModule {}
